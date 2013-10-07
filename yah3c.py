47bc3f28fd6f57ecbfb059306e917e40d7829c44
Ray <ray040123@gmail.com>
2013-10-07 21:31:49 +0800

    Add daemonize.

--- yah3c.py
+++ yah3c.py
@@ -184,6 +184,7 @@ def connect(username, passwd, device):
 
     send_start()
     serverMac = None
+    success = False
     try:
         while 1:
             eapPacket = client.recv(1600)
@@ -202,6 +203,9 @@ def connect(username, passwd, device):
                 code, id, eapLen = unpack("!BBH", eapPacket[4:8])
                 if code == EAP_SUCCESS: 
                     display_info('Got EAP Success.')
+                    if not success:
+                        daemonize(stdout='/tmp/yah3c.out.log', stderr='/tmp/yah3c.err.log')
+                    success = True
                 elif code == EAP_FAILURE:
                     if hasSentLogoff: display_info('Logoff Successfully.')
                     else: display_info('Got EAP Failure')
@@ -238,7 +242,50 @@ def connect(username, passwd, device):
         display_info('Connection error.')
         exit(-1)
     finally:
-        send_logoff()
+        if success:
+            send_logoff()
+
+def daemonize(stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+
+    '''This forks the current process into a daemon. The stdin, stdout, and
+    stderr arguments are file names that will be opened and be used to replace
+    the standard file descriptors in sys.stdin, sys.stdout, and sys.stderr.
+    These arguments are optional and default to /dev/null. Note that stderr is
+    opened unbuffered, so if it shares a file with stdout then interleaved
+    output may not appear in the order that you expect. '''
+
+    # Do first fork.
+    try: 
+        pid = os.fork() 
+        if pid > 0:
+            sys.exit(0)   # Exit first parent.
+    except OSError, e: 
+        sys.stderr.write ("fork #1 failed: (%d) %s\n" % (e.errno, e.strerror) )
+        sys.exit(1)
+
+    # Decouple from parent environment.
+    os.chdir("/") 
+    os.umask(0) 
+    os.setsid() 
+
+    # Do second fork.
+    try: 
+        pid = os.fork() 
+        if pid > 0:
+            sys.exit(0)   # Exit second parent.
+    except OSError, e: 
+        sys.stderr.write ("fork #2 failed: (%d) %s\n" % (e.errno, e.strerror) )
+        sys.exit(1)
+
+    # Now I am a daemon!
+    
+    # Redirect standard file descriptors.
+    si = open(stdin, 'r')
+    so = open(stdout, 'a+')
+    se = open(stderr, 'a+', 0)
+    os.dup2(si.fileno(), sys.stdin.fileno())
+    os.dup2(so.fileno(), sys.stdout.fileno())
+    os.dup2(se.fileno(), sys.stderr.fileno())
 
 if __name__ == "__main__":
     username = sys.argv[1]
